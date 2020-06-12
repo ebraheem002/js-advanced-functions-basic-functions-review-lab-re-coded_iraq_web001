7be3b385ce53162ce231a85564b09b2b27c6d7ab
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-12 10:16:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+
+function saturdayFun(MyActivity = "roller-skate"){
+  return `This Saturday, I want to ${MyActivity}!`
+}
+saturdayFun()
+saturdayFun('I want to bathe my dog')
+
+let mondayWork = function (MyActivity1 = "go to the office") {
+  return `This Monday, I will ${}`
+}
+
+mondayWork('Play basketball with friend')
